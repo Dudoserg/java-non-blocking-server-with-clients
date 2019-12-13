a58d999d832ecc7938b3b20419d9fc1e26a44614
dudoser <frielender@gmail.com>
2019-12-13 23:25:58 +0700

    first commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,14 @@
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Server {
+
+
+
+    public Server() {
+
+    }
+}
